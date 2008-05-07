618b474e093caac6037b1befdf551bedf2d203e8
Carlos Garcia Campos <carlosgc@gsyc.escet.urjc.es>
2008-05-07 09:28:36 +0000

    Initial bazar support.
    2008-05-07  Carlos Garcia Campos  <carlosgc@gsyc.escet.urjc.es>
    
    	* repositoryhandler/backends/Makefile.am:
    	* repositoryhandler/backends/bzr.py:
    	* tests/Makefile.am:
    	* tests/bzr.py:
    
    	Initial bazar support.

---
+++ repositoryhandler/backends/bzr.py
@@ -0,0 +1,158 @@
+# bzr.py
+#
+# Copyright (C) 2008 Carlos Garcia Campos <carlosgc@gsyc.escet.urjc.es>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import re
+import os
+
+from repositoryhandler.Command import Command, CommandError
+from repositoryhandler.backends import Repository, RepositoryInvalidWorkingCopy, register_backend
+from repositoryhandler.backends.watchers import *
+
+def get_repository_from_path (path):
+    if os.path.isfile (path):
+        path = os.path.dirname (path)
+
+    pattern = re.compile ("^[ \t]*parent branch:(.*)$")
+    uri = None
+    
+    try:
+        cmd = ['bzr', 'info']
+
+        command = Command (cmd, path, env = {'LC_ALL' : 'C'})
+        out = command.run_sync ()
+
+        for line in out.splitlines ():
+            match = pattern.match (line)
+            if not match:
+                continue
+            
+            uri = match.group (1).strip ()
+            break
+        
+    except CommandError:
+        raise RepositoryInvalidWorkingCopy ('"%s" does not appear to be a Bzr working copy' % path)
+
+    if uri is None:
+        raise RepositoryInvalidWorkingCopy ('"%s" does not appear to be a Bzr working copy' % path)
+
+    return 'bzr', uri
+
+class BzrRepository (Repository):
+    '''Bazar Repository'''
+
+    def __init__ (self, uri):
+        Repository.__init__ (self, uri, 'bzr')
+
+    def _check_uri (self, uri):
+        type, repo_uri = get_repository_from_path (uri)
+        if not repo_uri.startswith (self.uri):
+            raise RepositoryInvalidWorkingCopy ('"%s" does not appear to be a Bzr working copy '
+                                                '(expected %s but got %s)' % (uri, self.uri, repo_uri))
+
+    def checkout (self, module, rootdir, newdir = None, branch = None, rev = None):
+        # branch doesn't make sense here module == branch
+        if newdir is not None:
+            srcdir = os.path.join (rootdir, newdir)
+        elif newdir == '.':
+            srcdir = rootdir
+        else:
+            srcdir = os.path.join (rootdir, module)
+        if os.path.exists (srcdir):
+            try:
+                self.update (srcdir, rev)
+                return
+            except RepositoryInvalidWorkingCopy:
+                # If srcdir is not a valid working copy,
+                # continue with the checkout
+                pass
+
+        cmd = ['bzr', 'branch', self.uri]
+
+        if newdir is not None:
+            cmd.append (newdir)
+        else:
+            cmd.append (module)
+
+        command = Command (cmd, rootdir)
+        self._run_command (command, CHECKOUT)
+
+    def update (self, uri, rev = None):
+        self._check_uri (uri)
+
+        #TODO: revision
+        
+        cmd = ['bzr', 'pull']
+
+        if os.path.isfile (uri):
+            directory = os.path.dirname (uri)
+        else:
+            directory = uri
+
+        command = Command (cmd, directory)
+        self._run_command (command, UPDATE)
+        
+    def log (self, uri, branch = None, rev = None, files = None):
+        self._check_uri (uri)
+
+        if os.path.isfile (uri):
+            cwd = os.path.dirname (uri)
+            files = [os.path.basename (uri)]
+        elif os.path.isdir (uri):
+            cwd = uri
+            files = ['.']
+        else:
+            cwd = os.getcwd ()
+        
+        cmd = ['bzr', 'log', '-v']
+
+        #TODO: branch
+
+        if files is not None:
+            for file in files:
+                cmd.append (file)
+        else:
+            cmd.append (uri)
+
+        command = Command (cmd, cwd)
+        self._run_command (command, LOG)
+
+    def diff (self, uri, branch = None, revs = None, files = None):
+        # TODO
+        pass
+
+    def get_modules (self):
+        #Not supported by Bzr
+        return []
+
+    def get_last_revision (self, uri):
+        self._check_uri (uri)
+
+        cmd = ['bzr', 'revno']
+
+        command = Command (cmd, uri)
+        try:
+            out = command.run_sync ()
+        except:
+            return None
+
+        if out == "":
+            return None
+
+        return out.strip ('\n\t ')    
+
+register_backend ('bzr', BzrRepository)
---
+++ tests/bzr.py
@@ -0,0 +1,81 @@
+import os
+import re
+from repositoryhandler.backends import create_repository, create_repository_from_path, RepositoryUnknownError
+from repositoryhandler.backends.watchers import *
+from tests import Test, register_test, remove_directory
+
+class BzrTest (Test):
+
+    def checkout (self):
+        self.repo = create_repository ('bzr', 'http://pkg-config.freedesktop.org/bzr/')
+        self.repo.checkout ('pkg-config', '/tmp/', newdir = 'pkg-config')
+        if not os.path.exists ('/tmp/pkg-config/.bzr'):
+            print "Bzr checkout: FAILED"
+            return
+
+        try:
+            repo2 = create_repository_from_path ('/tmp/pkg-config')
+        except:
+            print "Bzr create_repository_from_path: FAILED"
+            return
+
+        print "Bzr checkout: PASSED"
+
+    def update (self):
+        try:
+            self.repo.update ('/tmp/pkg-config')
+        except:
+            print "Bzr update: FAILED"
+            return
+
+        # TODO
+        #try:
+        #    self.repo.update ('/tmp/pciutils/', rev = 'master')
+        #except:
+        #    print "Git update: FAILED"
+        #    raise
+        
+        print "Bzr update: PASSED"
+
+    def log (self):
+        def log_cb (data, user_data = None):
+            self.log_data += data
+
+        self.repo.add_watch (LOG, log_cb)
+
+        try:
+            self.log_data = ""
+            self.repo.log ('/tmp/pkg-config/')
+        except:
+            print "Bzr log: FAILED"
+            raise
+            return
+
+        if len (self.log_data) <= 0:
+            print "Bzr log: FAILED"
+            return
+
+        print "Bzr log: PASSED"
+
+    def diff (self):
+        # TODO
+        pass
+
+    def get_modules (self):
+        pass
+
+    def get_last_revision (self):
+        try:
+            rev = self.repo.get_last_revision ('/tmp/pkg-config')
+            if rev is not None:
+                print "Bzr get_last_revision (%s): PASSED" % (rev)
+                return
+        except:
+            pass
+
+        print "Bzr get_last_revision: FAILED"
+
+    def clean (self):
+        remove_directory ('/tmp/pkg-config')
+
+register_test ('bzr', BzrTest)    
