cfd7476f4d403f20afb1af9eccf4f37b3da9ebce
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2011-12-19 00:43:47 +0100

    Reverting commit 6c25e76c5d4e1459188587369d8369b22890cf2a which broke how lines are read when a temporary buffer ends in the mid of a line. This closes bug #318  but reopens #300.

--- repositoryhandler/Command.py
+++ repositoryhandler/Command.py
@@ -191,10 +191,8 @@ class Command:
         def out_cb(out_chunk, out_data_l):
             out_data = out_data_l[0]
             out_data += out_chunk
-            while len(out_data) > 0:
+            while '\n' in out_data:
                 pos = out_data.find('\n')
-                if pos < 0:
-                    pos = len(out_data)
                 parser_out_func(out_data[:pos + 1])
                 out_data = out_data[pos + 1:]
             out_data_l[0] = out_data
@@ -202,10 +200,8 @@ class Command:
         def err_cb(err_chunk, err_data_l):
             err_data = err_data_l[0]
             err_data += err_chunk
-            while len(err_data) > 0:
+            while '\n' in out_data:
                 pos = err_data.find('\n')
-                if pos < 0:
-                    pos = len(err_data)
                 parser_error_func(err_data[:pos + 1])
                 err_data = err_data[pos + 1:]
             err_data_l[0] = err_data
